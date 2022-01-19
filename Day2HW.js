75c63f20db06237c8fe737811991c65a5b17d696
Deyvin Cano <deyvin@Deyvins-MacBook-Air.local>
2022-01-19 14:33:57 -0500

    first git commit

---
+++ Day2HW.js
@@ -0,0 +1,49 @@
+//const testArray = [ true, false, false, true, false, false, false, true, true, true, false ]
+// counts = 0;
+// for(i = 0; i < testArray.length; i++){
+//     if(testArray[i]===true){
+//         counts++
+//     }
+// }
+// console.log(counts);
+
+
+
+// Second part of HW
+// const car= {
+//     make: 'Camero',
+//     year: '2014'
+// }
+// function myCar(car){
+//     console.log(car.make, car.year);
+// }
+// myCar(car)
+
+
+
+
+// Third part of the HW
+// const food = ['chinese','chipotle', 'thia','chilis'];
+// function myFood(food){
+//     console.log(food[0])
+
+// }
+// myFood(food)
+
+
+
+// New way to do hw
+const testArray = [ true, false, false, true, false, false, false, true, true, true, false ]
+
+function countTrues(arr){
+let count = 0
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === true){
+            count++;
+
+        }
+    }
+    return count;
+
+}
+console.log(countTrues(testArray))
---
+++ variable.js
@@ -0,0 +1,4 @@
+const country = 'USA'
+const continent = 'North America'
+const population = '310_000_000'
+console.log('Country', country)
