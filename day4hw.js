3d33b3415b048928bd3b1faadc22e7eb341a1c3b
Deyvin Cano <deyvin@Deyvins-MacBook-Air.local>
2022-01-21 12:50:47 -0500

    learning git

--- day4hw.js
+++ day4hw.js
@@ -1,32 +1,42 @@
 
-// first part of hw
+// // first part of hw
 
-for(i=10; i > 0; i--){
-    console.log(i)
-}
-console.log ('BLAST OFF!')
+// for(i=10; i > 0; i--){
+//     console.log(i)
+// }
+// console.log ('BLAST OFF!')
 
 
-//second  part
-function findMinMax(arr){
-    let min = arr[0]
-    let max = arr[0]
-    for(let i = 0; i < arr.length; i ++){
-        if(min > arr[i]){
-            min = arr[i]
-        }
-        if(arr[i]> max){
-            max = arr[i]
-        }
+// //second  part
+// function findMinMax(arr){
+//     let min = arr[0]
+//     let max = arr[0]
+//     for(let i = 0; i < arr.length; i ++){
+//         if(min > arr[i]){
+//             min = arr[i]
+//         }
+//         if(arr[i]> max){
+//             max = arr[i]
+//         }
 
-    }
+    // }
+    
+    //     return [min,max]
+    // }
+    // const array = [23,1,56,7,8,4]
+    // console.log(...findMinMax(array))
+    
+    // //Third part of hw
+    // let string = "WHAT a TIME to BE ALIVE!"
+    // let result = string.toUpperCase();
+    // console.log(result)
+    
+    
+    
+    
+    const str24 = "something \n something else"
+    console.log(str24.indexOf('thing'))
+    console.log(str24.split(''))
 
-    return [min,max]
-}
-const array = [23,1,56,7,8,4]
-console.log(...findMinMax(array))
-
-//Third part of hw
-let string = "WHAT a TIME to BE ALIVE!"
-let result = string.toUpperCase();
-console.log(result)
+    // vitoria is cool
+    // manny is kind cool
