d7920e6679d83e31056c31f7c7e8dc43de9eeeff
“Deyvin <deyvinvelasquez0202@gmail.com>
2022-01-23 20:37:56 -0500

    Practicing Code

---
+++ practice.js
@@ -0,0 +1,32 @@
+// Create a function that takes two numbers as arguments and return their sum.
+
+function totalSum(a,b){
+    let sum = a + b
+    return sum
+}
+console.log(totalSum(2,4))
+
+
+
+
+// Create a function that takes voltage and current and returns the calculated power.
+
+function calPower(volt,curr){
+    let power = volt * curr
+    return power
+}
+console.log(calPower(10,4))
+
+
+
+// Write a function that accepts an array of strings. Return the longest string.
+// In progress...
+// function longestString(arr){
+//     arr = ['Remember', 'Today']
+//     for(i = 0; i < arr.length; i++){
+//         return Math.max(string)
+
+//     }
+
+// }
+// console.log(longestString(arr))
